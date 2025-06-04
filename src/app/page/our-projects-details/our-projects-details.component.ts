5dcc260d3088129230e5ca21292df051ac2894fe
prashantkashyap12 <prashantkashyap12@gmail.com>
2025-06-04 22:13:43 +0530

    proj_dy

--- src/app/page/our-projects-details/our-projects-details.component.ts
+++ src/app/page/our-projects-details/our-projects-details.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-our-projects-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './our-projects-details.component.html',
   styleUrl: './our-projects-details.component.css'
 })
 export class OurProjectsDetailsComponent {
+
+
+  projDetails1:any[] = [
+    {img1:'assets/img/project-details-img-1.jpg', img2:'assets/img/project-details-img-1.jpg', img3:'assets/img/project-details-img-1.jpg'}
+  ]
+  projDetails2:any[]=[
+    {head1:'Child trouble & care', pera1:'Netus lorem rutrum arcu dignissim at sit morbi phasellus nascetur eget potenti vestibulum is cras. Tempor nonummy metus lobortis. Sociis velit etiam, dapibus.Lectus ve pellentesque a cras posuere tempor facilisi habitant lectus rutrum pede quisque hendrerit mauris ad to elementum fringilla facilisi volutpat fusce pharetra.', pera11:'Netus lorem rutrum arcu dignissim at sit morbi phasellus nascetur eget potenti vestibulum is cras. Tempor nonummy metus lobortis. Sociis velit etiam, dapibus.Lectus ve pellentesque a cras posuere tempor facilisi habitant lectus rutrum pede . Netus lorem rutrum arcu dignissim at sit morbi phasellus nascetur eget potenti vestibulum is cras.', head2:'Challenge & Solution ', pera2:'Future, as it seeks to lead the industry in technological innovation and sustainable building practices to deliver long-lasting value for its clients. Netus lorem rutrum arcu dignissim at sit morbi nascetur eget potenti vestibulum is cras. Tempor nonummy metus lobortis. Sociis velit etiam, dapibus.Lepellentesque a cras posuere tempor facilisi habitant lectus rutrum pede .', head3:'Final Result', pera3:'For almost 50 years Leighton Asia, one of the region’s largest and most respected construc companies, has been progressively building for a better future by leveraging international expertise with local intelligence. In that time Leighton.'}
+  ]
+  projInfo:any[] = [
+    {cat:'Business, Marketing', auth:'Athena Jones', tag:'Data Masters', cost:'USD 1,50,499', date:'25 February, 2024'}
+  ]
 
 }
