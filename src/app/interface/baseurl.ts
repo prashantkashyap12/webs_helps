33ec79c86220d5ab9aa715e117e67ab8c5797f3c
prashantkashyap12 <prashantkashyap12@gmail.com>
2025-08-02 08:16:09 +0530

    update 2nd

--- src/app/interface/baseurl.ts
+++ src/app/interface/baseurl.ts
@@ -1,3 +1,4 @@
 export class baseurl  {
-    public baseUrl:string = "http://localhost:5181/"; // api_path
+    // public baseUrl:string = "http://localhost:5181/"; // api_path
+    public baseUrl:string = "https://3g40jt7l-5181.inc1.devtunnels.ms/"
 }
